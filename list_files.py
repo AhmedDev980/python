49ce63e5195efa09cabd7c2170fd029d5f087665
AhmedDev980 <sk.khwajaahmed.me@gmail.com>
2025-03-17 15:23:32 +0530

    Update list_files.py

--- list_files.py
+++ list_files.py
@@ -2,15 +2,15 @@
 
 import os
 # .split() is used to convert the folder names into list
-folers = input("Enter the foler names:" ).split()
+folders = input("Enter the folder names:" ).split()
 
 for folder in folders:
  try:
    files = os.listdir(folder)
- except FileNotFoundError
+ except FileNotFoundError:
   print ("Please provide the valid folder name. Looks loke this folder does not exist:" + folder )
   break
  # if we want to stop and comeout of the program after facing error we can provide break, if want to proceed with next folder names provide continue
-  print("The list of files in the folder:" +folder )
-  for file in files:
+ print("The list of files in the folder:" +folder )
+ for file in files:
     print(file)
